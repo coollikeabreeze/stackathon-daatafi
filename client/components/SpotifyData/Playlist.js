9ac3c3d48d20fb8ab0cdc9f66a7787970dc338f2
Andrea Khan <khan.andrea@gmail.com>
2022-01-23 15:51:22 -0500

    added search song/artist from Spotify function

--- client/components/SpotifyData/Playlist.js
+++ client/components/SpotifyData/Playlist.js
@@ -1,21 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
 
-const DropDowns = (props) => {
-  const { playlistName, playlistImage, playlistDescription, playlistUrl } =
+const Playlist = (props) => {
+
+  const { playlistName, playlistImage, playlistDescription, playlistUrl , playlistTracks} =
     props.playlist;
+
+  console.log(playlistTracks)
+
+  const [lyrics, setLyrics] = useState([])
+  const [token, setToken] = useState("");
+
+//  const getLyrics = (songTitle) => {
+//   axios(`https://api.genius.com?q=${songTitle}`, {
+//     method: "GET",
+//     headers: {
+//       Authorization: "Bearer " + process.env.geniusClientAccessToken,
+//     },
+//   })
+
+//     .then((lyricsResponse) => {
+//       console.log(lyricsResponse);
+//       // setLyrics({
+//       // });
+//     });
+//  }
+
+  useEffect(() => {
+      axios.get(
+        `https://api.genius.com/oauth/authorize?client_id=${process.env.geniusClientId}&redirect_uri=${process.env.REDIRECT_URI}&scope=GET&response_type=code`
+      )
+
+        .then((lyricsResponse) => {
+          console.log(lyricsResponse);
+          // setLyrics({
+          // });
+        });
+
+        // setLyrics({
+        //   playlistName: playlistResponse.data.name,
+        //   playlistImage: playlistResponse.data.images[0].url,
+        //   playlistDescription: playlistResponse.data.description,
+        //   playlistUrl: playlistResponse.data.external_urls.spotify,
+        //   playlistTracks: songsArr
+        // });
+  }, [])
 
   return (
     <div>
       <div className="container flex flex-row m-6">
-        <img className="object-cover h-20 flex-" src={playlistImage} />
-        <div className="container flex-col">
+        <img className="object-cover flex h-28" src={playlistImage} />
+        <div className="container flex-col m-3">
           <h1 className="text-3xl">{playlistName}</h1>
           <p>{playlistDescription}</p>
         </div>
       </div>
+
+      <div className="container m-6 ">
+        <table className="table-fixed rounded-sm border-collapse bg-gray-900">
+          <thead>
+            <tr className="px-8 py-4">
+              <td className="w-16 px-8 py-2">Number</td>
+              <td className="p-6">Track</td>
+              <td className="p-6">Popularity</td>
+            </tr>
+          </thead>
+          <tbody className=" items-center justify-between overflow-y-scroll">
+          {playlistTracks.map((track,idx) => (
+          <tr key={idx} className=" items-center justify-between overflow-y-scroll">
+            <td className="px-8 py-2 border-t border-gray-600 p-8">{idx+1}</td>
+            <td className="px-8 py-2 border-t border-gray-600 p-8">{track.trackName}</td>
+            <td className="px-8 py-2 border-t border-gray-600 p-8">{track.trackPopularity}</td>
+          </tr>
+        ))}
+        </tbody>
+        </table>
+      </div>
+
     </div>
   );
 };
 
-export default DropDowns;
+export default Playlist;
+
--- client/components/SpotifyData/index.js
+++ client/components/SpotifyData/index.js
@@ -5,21 +5,51 @@ import useAuth from "./useAuth";
 
 import DropDowns from "./DropDowns";
 import Playlist from "./Playlist";
+import SearchResults from "./SearchResults"
 import SpotifyWebApi from "spotify-web-api-node/src/spotify-web-api";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID
+})
 
 const SpotifyData = ({code}) => {
 
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID
-  })
-
   const accessToken = useAuth(code)
   const [search,setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+
 
   useEffect(() => {
     if (!accessToken) return
     spotifyApi.setAccessToken(accessToken)
   },[accessToken])
+
+  useEffect(() => {
+    if(!search) return setSearchResults([])
+    if(!accessToken) return
+
+    let cancel = false
+    spotifyApi.searchTracks(search).then(res => {
+      if (cancel) return
+      setSearchResults(res.body.tracks.items.map(track=> {
+        const smallestAlbumImage = track.album.images.reduce(
+          (smallest, image) => {
+            if (image.height < smallest.height) return image
+            return smallest
+          }, track.album.images[0])
+
+        return{
+          artist: track.artists[0].name,
+          title: track.name,
+          uri: track.uri,
+          albumUrl: smallestAlbumImage.url,
+          popularity: track.popularity
+        }
+      }))
+    })
+
+    return () => cancel = true
+  }, [search, accessToken])
 
   return (
     <div>
@@ -28,8 +58,11 @@ const SpotifyData = ({code}) => {
      value={search}
      onChange={(e)=> setSearch(e.target.value)}></input>
 
+    <SearchResults searchResults={searchResults}/>
+
+
     </div>
-  );
+  )
 };
 
 export default SpotifyData;
