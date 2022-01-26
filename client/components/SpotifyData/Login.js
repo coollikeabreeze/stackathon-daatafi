8718428cf9bd8b4e85322525ffcfc1011131918b
Andrea Khan <khan.andrea@gmail.com>
2022-01-25 19:05:01 -0500

    minor edits

--- client/components/SpotifyData/Login.js
+++ client/components/SpotifyData/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=416fece6750a4a9bb9185f0be748671c&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=416fece6750a4a9bb9185f0be748671c&response_type=code&redirect_uri=http://localhost:8080&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
   return (
